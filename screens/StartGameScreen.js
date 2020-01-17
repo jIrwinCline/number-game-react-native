22a12a104f96ca24a2b9b5c6f036cb10a9c810c8
jirwincline <jakobinwins@gmail.com>
2020-01-16 17:40:42 -0800

    only able to put numeric input now, and keyboard is dismissed when the screen in touched

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 import Card from '../components/Card';
 import Input from '../components/Input';
 import Colors from '../constants/colors'; 
 
 const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] = useState('')
+
+    const numberInputHandler = inputText => {
+      setEnteredValue(inputText.replace(/[^0-9]/g, ''))
+    };
+
     return (
-      <View style={styles.screen}>
-        <Text style={styles.title}>Start a New Game!</Text>
-        <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
-          <Input style={styles.input} blurOnSubmit keyboardType='numeric' maxLength={2}/>
-          <View style={styles.buttonContainer}>
-            <View style={styles.button}>
-              <Button color={Colors.accent} title="reset" onPress={() => {}} />
+      <TouchableWithoutFeedback onPress={() => {
+        Keyboard.dismiss();
+      }}>
+        <View style={styles.screen}>
+          <Text style={styles.title}>Start a New Game!</Text>
+          <Card style={styles.inputContainer}>
+            <Text>Select a Number</Text>
+            <Input style={styles.input} blurOnSubmit keyboardType='numeric' maxLength={2} onChangeText={numberInputHandler} value={enteredValue}/>
+            <View style={styles.buttonContainer}>
+              <View style={styles.button}>
+                <Button color={Colors.accent} title="reset" onPress={() => {}} />
+              </View>
+              <View style={styles.button}>
+                <Button color={Colors.primary} title="Confirm" onPress={() => {}} />
+              </View>
             </View>
-            <View style={styles.button}>
-              <Button color={Colors.primary} title="Confirm" onPress={() => {}} />
-            </View>
-          </View>
-        </Card>
-      </View>
+          </Card>
+        </View>
+      </TouchableWithoutFeedback>
     );
 }
 
