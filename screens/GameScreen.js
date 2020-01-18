4701628077854b0ef908629423a5e88955f1d00c
jirwincline <jakobinwins@gmail.com>
2020-01-17 18:50:19 -0800

    fixed scroll issue with flexGrow: 1 style

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -83,8 +83,8 @@ const GameScreen = props => {
           <Ionicons name="md-add" size={24} color="white" />
         </MainButton>
       </Card>
-      <View style={styles.list}>
-        <ScrollView>
+      <View style={styles.listContainer}>
+        <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) =>
             renderListItem(guess, pastGuesses.length - index)
           )}
@@ -107,9 +107,14 @@ const styles = StyleSheet.create({
     width: 400,
     maxWidth: "90%"
   },
-  list: {
+  listContainer: {
     width: "80%",
     flex: 1
+  },
+  list: {
+    alignItems: "center",
+    justifyContent: "flex-end",
+    flexGrow: 1
   },
   listItem: {
     borderColor: "#ccc",
@@ -118,7 +123,8 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     backgroundColor: "white",
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    width: "60%"
   }
 });
 
